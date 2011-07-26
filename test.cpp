42f25dea5018fc3cd8a8afea07e3d2c3267990ad
Leonardo Boquillón <lboquillon@users.noreply.github.com>
2011-07-25 19:32:35 -0530

    added GPLv3 heading

--- test.cpp
+++ test.cpp
@@ -1,4 +1,24 @@
-// Copyright (C) FuDePAN 2011
+/*
+test.cpp: Nucleotides Compression Algorithms
+    Copyright (C) 2011 Daniel Gutson and Leonardo Boquillon, FuDePAN
+
+    This file is part of Nuca.
+
+    Aso is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Nuca is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Nuca. If not, see <http://www.gnu.org/licenses/>.
+
+    NOTE: This file is in prototype stage, and is under active development.
+*/
 
 #include "comp.h"
 
