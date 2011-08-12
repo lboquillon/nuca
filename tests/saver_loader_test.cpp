664a67ad13064b9ec69e1daadd8d01bc79504c7d
Leonardo Boquillón <lboquillon@users.noreply.github.com>
2011-08-12 15:37:49 -0530

    Added test file

---
+++ tests/saver_loader_test.cpp
@@ -0,0 +1,33 @@
+#include <gtest/gtest.h>
+#include <biopp/bio_molecular/bio_molecular.h>
+#include "../comp.h"
+
+TEST(saver_loader, equalStr1)
+{
+    const NucSequence in("ATTCCTTGTGGTTTCCAAGGTTCTCTCTAGTCCTGAC");
+    NucSequence out;
+    Saver saver("archivo.out");
+    Storer<Saver> storer(saver, in.length());
+    storeSeq(storer, in);
+
+    Loader loader("archivo.out");
+    Retriever<Loader> retriever(loader);
+    loadSeq(retriever, out);
+
+    ASSERT_EQ(in, out);
+}
+
+TEST(saver_loader, equalStr2)
+{
+    const NucSequence in("CCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAACCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTGGGGGGGGGGGGGGGGGGGGGGGGGGGACTGAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAACCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGTCAGTCA");
+    NucSequence out;
+    Saver saver("archivo.out");
+    Storer<Saver> storer(saver, in.length());
+    storeSeq(storer, in);
+
+    Loader loader("archivo.out");
+    Retriever<Loader> retriever(loader);
+    loadSeq(retriever, out);
+
+    ASSERT_EQ(in, out);
+}
