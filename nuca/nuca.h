729d79d57bc68ecc3ff4cca4cd8e4360897aa82b
Leonardo Boquillon <lboquillon@gmail.com>
2011-08-20 09:04:25 -0530

    Added gpl header

--- nuca/nuca.h
+++ nuca/nuca.h
@@ -1,3 +1,25 @@
+/*
+nuca.h: Nucleotides Compression Algorithms
+    Copyright (C) 2011 Leonardo Boquillon and Daniel Gutson, FuDePAN
+
+    This file is part of Nuca.
+
+    Nuca is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Nuca is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Nuca. If not, see <http://www.gnu.org/licenses/>.
+
+    NOTE: This file is in prototype stage, and is under active development.
+*/
+
 #ifndef NUCA_H
 #define NUCA_H
 
