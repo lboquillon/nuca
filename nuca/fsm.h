8aad38b14bd8360ebdf5dc0f447ac416bd0a4891
Leonardo Boquillon <lboquillon@gmail.com>
2011-08-20 08:34:19 -0530

    added fsm superclass and minor fixes

--- nuca/fsm.h
+++ nuca/fsm.h
@@ -1,9 +1,31 @@
 #ifndef FSM_H
 #define FSM_H
 
+#include <string>
+
 enum EndSeqStimulus
 {
-	EndSeq
+    EndSeq
+};
+
+class Fsm
+{
+protected:
+    std::string rareSeq;
+    std::string& outSeq;
+
+    void addMissingNuc(size_t n)
+    {
+        outSeq += rareSeq.substr(0, n);
+    }
+
+public:
+    Fsm(std::string& out)
+        : rareSeq("TTT")
+        , outSeq(out)
+    {
+    }
+
 };
 
 #endif
