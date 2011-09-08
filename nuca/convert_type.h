67248ac4cdf8f6cb8caa5345a79d8e2d8d1d4f0b
Leonardo Boquillon <lboquillon@gmail.com>
2011-09-08 17:55:30 -0530

    Rename saver by ostrea_saver, added set method to add ostream, changed the escape sequence

--- nuca/convert_type.h
+++ nuca/convert_type.h
@@ -1,8 +1,30 @@
+/*
+convert_type.h: Nucleotides Compression Algorithms
+    Copyright (C) 2011 Leonardo Boquillon and Daniel Gutson, FuDePAN
+
+    This file is part of Nuca.
+
+    Nuca is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Nuca is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Nuca. If not, see <http://www.gnu.org/licenses/>.
+
+    NOTE: This file is in prototype stage, and is under active development.
+*/
+
 #ifndef CONVERT_TYPE
 #define CONVERT_TYPE
 
 #include "end_layer.h"
-#include "saver.h"
+#include "ostream_saver.h"
 #include "bit_handle.h"
 #include "tests_layers.h"
 #include "remove_ns.h"
@@ -12,10 +34,10 @@ template<class UpperLayer, class LowerLayer>
 class ConvertDataType;
 
 template<>
-class ConvertDataType<StringTestLayer<CompressingBitHandle<Saver<EndLayer> > >, CompressingBitHandle<Saver<EndLayer> > >
+class ConvertDataType<StringTestLayer<CompressingBitHandle<OstreamSaver<EndLayer> > >, CompressingBitHandle<OstreamSaver<EndLayer> > >
 {
 private:
-    typedef CompressingBitHandle<Saver<EndLayer> > Lower;
+    typedef CompressingBitHandle<OstreamSaver<EndLayer> > Lower;
 public:
     static typename Lower::DataType convert(typename CompressingBitHandle<Lower>::DataType data)
     {
@@ -23,11 +45,11 @@ public:
     }
 };
 
-template<>        // RemoveNs<StringTestLayer<CompressingBitHandle<Saver<EndLayer> > > > b;
-class ConvertDataType<RemoveNs<StringTestLayer<CompressingBitHandle<Saver<EndLayer> > > >, StringTestLayer<CompressingBitHandle<Saver<EndLayer> > > >
+template<>
+class ConvertDataType<RemoveNs<StringTestLayer<CompressingBitHandle<OstreamSaver<EndLayer> > > >, StringTestLayer<CompressingBitHandle<OstreamSaver<EndLayer> > > >
 {
 private:
-    typedef StringTestLayer<CompressingBitHandle<Saver<EndLayer> > > Lower;
+    typedef StringTestLayer<CompressingBitHandle<OstreamSaver<EndLayer> > > Lower;
 public:
     static typename Lower::DataType convert(typename CompressingBitHandle<Lower>::DataType data)
     {
@@ -36,10 +58,10 @@ public:
 };
 
 template<>
-class ConvertDataType<RemoveNs<CompressingBitHandle<Saver<EndLayer> > >, CompressingBitHandle< Saver<EndLayer> > >
+class ConvertDataType<RemoveNs<CompressingBitHandle<OstreamSaver<EndLayer> > >, CompressingBitHandle< OstreamSaver<EndLayer> > >
 {
 private:
-    typedef CompressingBitHandle< Saver<EndLayer> > Lower;
+    typedef CompressingBitHandle< OstreamSaver<EndLayer> > Lower;
 
 public:
     static typename Lower::DataType convert(typename RemoveNs<Lower>::DataType data)
@@ -49,10 +71,10 @@ public:
 };
 
 template<>
-class ConvertDataType<CompressingBitHandle<Saver<EndLayer> >, Saver<EndLayer> >
+class ConvertDataType<CompressingBitHandle<OstreamSaver<EndLayer> >, OstreamSaver<EndLayer> >
 {
 private:
-    typedef Saver<EndLayer> Lower;
+    typedef OstreamSaver<EndLayer> Lower;
 public:
     static typename Lower::DataType convert(typename CompressingBitHandle<Lower>::DataType data)
     {
