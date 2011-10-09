943904baa598e0b378adfc5366dd4b3d90b62cf7
Leonardo Boquillón <lboquillon@users.noreply.github.com>
2011-10-09 15:21:08 -0530

    Updated data

--- doc/data/compression.cpp
+++ doc/data/compression.cpp
@@ -2,22 +2,22 @@
 #include <iostream>
 #include <fstream>
 
-typedef RemoveNs<CompressionEventsCounterLayer<EndLayer> > Compressor;
+typedef RemoveNs<CompressingBitHandler<OstreamSaver<EndLayer> > > Compressor;
 
 int main(int argc, char* argv[])
 {
-	Compressor cmpr;
-	std::ifstream is;
-	char c;
+    Compressor cmpr;
+    std::ifstream is;
+    std::ofstream os(argv[2], std::ios_base::binary);
+    char c;
 
-	is.open (argv[1]);
+    is.open(argv[1]);
+    cmpr.setOstream(os);
 
-	while (is.get (c))
-		cmpr.receiveData (c);
+    while (is.get(c))
+        cmpr.receiveData(c);
 
-	cmpr.receiveData (nuca::EndSeq);
+    cmpr.receiveData(nuca::EndSeq);
 
-	std::cout << (cmpr.getOutputNucNumber() / 4);
-
-	return 0;
+    return 0;
 }
