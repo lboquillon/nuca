d268a239b8313ae3afa5eed75715ce7f528b4cb1
Leonardo Boquillon <lboquillon@gmail.com>
2011-11-19 20:11:25 -0530

    Changed service to a class with static method

--- gui/service.h
+++ gui/service.h
@@ -1,41 +1,17 @@
-#include <string>
-#include <cctype>
-#include "nuca/nuca.h"
-
 #ifndef SERVICE_H
 #define SERVICE_H
 
-void compression(const std::string& fileIn, const std::string& fileOut)
+#include <iostream>
+#include <fstream>
+#include "nuca.h"
+
+class Service
 {
-    std::ofstream os(fileOut.c_str(), std::ios_base::binary);
-    RemoveNs<CompressingBitHandler<OstreamSaver<EndLayer> > > compressor;
-    compressor.setOstream(os);
+public:
+    Service();
+    static void compression(const std::string& fileIn, const std::string& fileOut);
+    static void decompression(const std::string& fileIn, const std::string& fileOut);
 
-    std::ifstream is(fileIn.c_str());
-    char c;
-
-    while (is.get(c))
-    {
-        c = toupper(c);
-
-        if (c == 'C' || c == 'T' || c == 'G' || c == 'A' || c == 'N')
-            compressor.receiveData(c);
-    }
-
-    compressor.receiveData(nuca::EndSeq);
-}
-
-void decompression(const std::string& fileIn, const std::string& fileOut)
-{
-    IstreamLoader<DecompressingBitHandler<AddNs<OstreamSaver<EndLayer> > > > decompressor;
-
-    std::ifstream is(fileIn.c_str(), std::ios_base::binary);
-    std::ofstream os(fileOut.c_str());
-
-    decompressor.setIstream(is);
-    decompressor.setOstream(os);
-
-    decompressor.run();
-}
+};
 
 #endif
